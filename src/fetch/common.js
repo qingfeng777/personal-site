4e7b01079043d9cba839a7ecdb908207d965354c
liushengjie <liushengjie@t2clould.net>
2018-12-11 21:03:47 +0800

    add list func

--- src/fetch/common.js
+++ src/fetch/common.js
@@ -5,7 +5,9 @@
 //import config from '../../app-config'
 import config from '../config'
 
-export const api = {paas:"http://localhost:7878/ming"}  //config.api //window.CONFIG.mock ? window.CONFIG.mockApi : window.CONFIG.api
+export const api = {
+  paas: "http://localhost:7878/ming"
+} //config.api //window.CONFIG.mock ? window.CONFIG.mockApi : window.CONFIG.api
 
 import axios from 'axios'
 // axios.defaults.baseURL = config.loginApi
@@ -35,30 +37,30 @@ axios.interceptors.response.use(function (response) {
 // --method：请求方式 如：post、get等
 // --params：url参数
 // --data：发送数据对象
-export function fetch (url, method, data, params) {
+export function fetch(url, method, data, params) {
   return new Promise((resolve, reject) => {
     axios({
-      method: method || 'get',
-      url: url,
-      params: params || {},
-      data: data || {},
-      // responseType: 'json',
-      headers: {
-        // 序列化后的 JSON 字符串
-        'Content-Type': 'application/json'
-        //'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      withCredentials: true,
-      transformRequest: [function (data) {
-        // Do whatever you want to transform the data
-        let ret = ''
-        ret = JSON.stringify(data)
-        // for (const it in data) {
-        //   ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-        // }
-        return ret
-      }]
-    })
+        method: method || 'get',
+        url: url,
+        params: params || {},
+        data: data || {},
+        // responseType: 'json',
+        headers: {
+          // 序列化后的 JSON 字符串
+          'Content-Type': 'application/json'
+          //'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        withCredentials: true,
+        transformRequest: [function (data) {
+          // Do whatever you want to transform the data
+          let ret = ''
+          ret = JSON.stringify(data)
+          // for (const it in data) {
+          //   ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+          // }
+          return ret
+        }]
+      })
       .then(response => {
         resolve(response.data)
       })
--- src/store/moudle/article/index.js
+++ src/store/moudle/article/index.js
@@ -4,10 +4,10 @@ import article from '../../../fetch/article'
 
 // initial state
 const state = {
-  listData: [],       // 主列表数据
-  listPageNum: 1,     // 主列表页码
+  listData: [], // 主列表数据
+  listPageNum: 1, // 主列表页码
   listLoading: false, // 主列表载入状态
-  itemData: {}        // 当前详情数据
+  itemData: {} // 当前详情数据
 }
 // getters
 const getters = {
@@ -24,53 +24,63 @@ const getters = {
 }
 // actions
 const actions = {
-    // 获取列表
-  getList ({ commit, state }, searchKey) {
+  // 获取列表
+  getList({
+    commit,
+    state
+  }, searchKey) {
     commit(types.SET_LIST_LOADING, true)
     const limit = 10 //parseInt(Cookies.get('page-size'))
     const offset = (state.listPageNum - 1) * limit
     article._getList(limit, offset, searchKey)
-    .then(res => {
-      commit(types.SET_LIST_DATA, res['data'])
-      commit(types.SET_LIST_LOADING, false)
-    })
-    .catch((error) => {
-      console.log(error.response.data)
-      commit(types.SET_LIST_LOADING, false)
-    })
-  },
-    // 获取单条记录,传id则请求api数据，不传则清空
-  getItem ({ commit, state }, id) {
-    if (id) {
-      article._getItem(id)
       .then(res => {
-        commit(types.SET_ITEM_DATA, res['data'])
+        commit(types.SET_LIST_DATA, res['data'])
+        commit(types.SET_LIST_LOADING, false)
       })
       .catch((error) => {
-        console.log(error.msg)
+        console.log(error.response.data)
+        commit(types.SET_LIST_LOADING, false)
       })
+  },
+  // 获取单条记录,传id则请求api数据，不传则清空
+  getItem({
+    commit,
+    state
+  }, id) {
+    if (id) {
+      article._getItem(id)
+        .then(res => {
+          commit(types.SET_ITEM_DATA, res['data'])
+        })
+        .catch((error) => {
+          console.log(error.msg)
+        })
     } else {
       commit(types.SET_ITEM_DATA, {})
     }
   },
-    // 更改页码
-  listChangePage  ({ dispatch, commit, state }, num) {
+  // 更改页码
+  listChangePage({
+    dispatch,
+    commit,
+    state
+  }, num) {
     commit(types.LIST_CHANGE_PAGE, num)
   }
 }
 
 // mutations
 const mutations = {
-  [types.SET_LIST_DATA] (state, savedata) {
+  [types.SET_LIST_DATA](state, savedata) {
     state.listData = savedata
   },
-  [types.SET_ITEM_DATA] (state, savedata) {
+  [types.SET_ITEM_DATA](state, savedata) {
     state.itemData = savedata
   },
-  [types.SET_LIST_LOADING] (state, savedata) {
+  [types.SET_LIST_LOADING](state, savedata) {
     state.listLoading = savedata
   },
-  [types.LIST_CHANGE_PAGE] (state, savedata) {
+  [types.LIST_CHANGE_PAGE](state, savedata) {
     state.listPageNum = savedata
   }
 }
